3315232d7012defa46a410d8c4b87de1eb678c0c
Andre Joseph <ajcubeta@gmail.com>
2017-05-16 18:50:25 +0800

    Prepare first pagination sample

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,97 @@
+import React, { PropTypes } from 'react';
+
+const propTypes = {
+    items: PropTypes.array.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    initialPage: PropTypes.number
+}
+
+const defaultProps = {
+    initialPage: 1
+}
+
+class Pagination extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { pager: {} };
+    }
+
+    componentWillMount() {
+        this.setPage(this.props.initialPage);
+    }
+
+    setPage(page) {
+        var items = this.props.items;
+        var pager = this.state.pager;
+
+        if (page < 1 || page > pager.totalPages) {
+            return;
+        }
+
+        pager = this.getPager(items.length, page);
+
+        var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
+
+        this.setState({ pager: pager });
+
+        this.props.onChangePage(pageOfItems);
+    }
+
+    getPager(totalItems, currentPage, pageSize) {
+        currentPage = currentPage || 1;
+        pageSize = pageSize || 10;
+        var totalPages = Math.ceil(totalItems / pageSize);
+
+        var startPage, endPage;
+        if (totalPages <= 10) {
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            if (currentPage <= 6) {
+                startPage = 1;
+                endPage = 10;
+            } else if (currentPage + 4 >= totalPages) {
+                startPage = totalPages - 9;
+                endPage = totalPages;
+            } else {
+                startPage = currentPage - 5;
+                endPage = currentPage + 4;
+            }
+        }
+
+        var startIndex = (currentPage - 1) * pageSize;
+        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+        var pages = _.range(startPage, endPage + 1);
+
+        return {
+            totalItems: totalItems,
+            currentPage: currentPage,
+            pageSize: pageSize,
+            totalPages: totalPages,
+            startPage: startPage,
+            endPage: endPage,
+            startIndex: startIndex,
+            endIndex: endIndex,
+            pages: pages
+        };
+    }
+
+    render() {
+        var pager = this.state.pager;
+
+        return (
+            <ul className="pagination">
+                <li className={pager.currentPage === 1 ? 'disabled' : ''}>
+                    <a onClick={() => this.setPage(pager.currentPage - 1)}>Prev</a>
+                </li>
+                <li className={pager.currentPage === pager.totalPages ? 'disabled' : ''}>
+                    <a onClick={() => this.setPage(pager.currentPage + 1)}>Next</a>
+                </li>
+            </ul>
+        );
+    }
+}
+
+Pagination.propTypes = propTypes;
+Pagination.defaultProps
+export default Pagination;
