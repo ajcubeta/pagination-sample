a4579120cd06cb8a6080d647b809618fcb76cb8f
Andre Joseph <ajcubeta@gmail.com>
2017-05-30 18:31:01 +0800

    Adjust tabsize and create textfield

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,38 +2,38 @@ import React from 'react';
 import Pagination from './Pagination';
 
 class App extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
+    
+    var sampleItems = _.range(1, 151).map(i => { return { id: i, name: 'Andre Joseph ' + i }; });
+    
+    this.state = {
+      sampleItems: sampleItems,
+      pageOfItems: []
+    };
+    
+    this.onChangePage = this.onChangePage.bind(this);
+  }
 
-        var sampleItems = _.range(1, 151).map(i => { return { id: i, name: 'Andre Joseph ' + i }; });
+  onChangePage(pageOfItems) {
+ 		this.setState({ pageOfItems: pageOfItems });
+  }
 
-        this.state = {
-            sampleItems: sampleItems,
-            pageOfItems: []
-        };
-
-        this.onChangePage = this.onChangePage.bind(this);
-    }
-
-    onChangePage(pageOfItems) {
-        this.setState({ pageOfItems: pageOfItems });
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <div className="text-center">
-                        <h1>Pagination Sample</h1>
-                        {this.state.pageOfItems.map(item =>
-                            <div key={item.id}>{item.name}</div>
-                        )}
-                        <Pagination items={this.state.sampleItems} onChangePage={this.onChangePage} />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="text-center">
+            <h1>Pagination Sample</h1>
+            {this.state.pageOfItems.map(item =>
+                <div key={item.id}>{item.name}</div>
+            )}
+            <Pagination items={this.state.sampleItems} onChangePage={this.onChangePage} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,93 +2,99 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-    items: PropTypes.array.isRequired,
-    onChangePage: PropTypes.func.isRequired,
-    initialPage: PropTypes.number
+  items: PropTypes.array.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  initialPage: PropTypes.number
 }
 
 const defaultProps = {
-    initialPage: 1
+  initialPage: 1
 }
 
 class Pagination extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { pager: {} };
+  constructor(props) {
+    super(props);
+    this.state = { pager: {} };
+  }
+
+  componentWillMount() {
+    this.setPage(this.props.initialPage);
+  }
+
+  setPage(page) {
+    var items = this.props.items;
+    var pager = this.state.pager;
+
+    if (page < 1 || page > pager.totalPages) { return; }
+
+    pager = this.getPager(items.length, page);
+
+    var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
+
+    this.setState({ pager: pager });
+
+    this.props.onChangePage(pageOfItems);
+  }
+
+  getPager(totalItems, currentPage, pageSize) {
+    currentPage = currentPage || 1;
+    pageSize = pageSize || 10;
+    var totalPages = Math.ceil(totalItems / pageSize);
+
+    var startPage, endPage;
+    if (totalPages <= 10) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      if (currentPage <= 6) {
+          startPage = 1;
+          endPage = 10;
+      } else if (currentPage + 4 >= totalPages) {
+          startPage = totalPages - 9;
+          endPage = totalPages;
+      } else {
+          startPage = currentPage - 5;
+          endPage = currentPage + 4;
+      }
     }
 
-    componentWillMount() {
-        this.setPage(this.props.initialPage);
-    }
+    var startIndex = (currentPage - 1) * pageSize;
+    var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+    var pages = _.range(startPage, endPage + 1);
 
-    setPage(page) {
-        var items = this.props.items;
-        var pager = this.state.pager;
+    return {
+      totalItems: totalItems,
+      currentPage: currentPage,
+      pageSize: pageSize,
+      totalPages: totalPages,
+      startPage: startPage,
+      endPage: endPage,
+      startIndex: startIndex,
+      endIndex: endIndex,
+      pages: pages
+    };
+  }
 
-        if (page < 1 || page > pager.totalPages) { return; }
+  render() {
+    var pager = this.state.pager;
 
-        pager = this.getPager(items.length, page);
-
-        var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
-
-        this.setState({ pager: pager });
-
-        this.props.onChangePage(pageOfItems);
-    }
-
-    getPager(totalItems, currentPage, pageSize) {
-        currentPage = currentPage || 1;
-        pageSize = pageSize || 10;
-        var totalPages = Math.ceil(totalItems / pageSize);
-
-        var startPage, endPage;
-        if (totalPages <= 10) {
-            startPage = 1;
-            endPage = totalPages;
-        } else {
-            if (currentPage <= 6) {
-                startPage = 1;
-                endPage = 10;
-            } else if (currentPage + 4 >= totalPages) {
-                startPage = totalPages - 9;
-                endPage = totalPages;
-            } else {
-                startPage = currentPage - 5;
-                endPage = currentPage + 4;
-            }
-        }
-
-        var startIndex = (currentPage - 1) * pageSize;
-        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-        var pages = _.range(startPage, endPage + 1);
-
-        return {
-            totalItems: totalItems,
-            currentPage: currentPage,
-            pageSize: pageSize,
-            totalPages: totalPages,
-            startPage: startPage,
-            endPage: endPage,
-            startIndex: startIndex,
-            endIndex: endIndex,
-            pages: pages
-        };
-    }
-
-    render() {
-        var pager = this.state.pager;
-
-        return (
-            <ul className="pagination">
-                <li className={pager.currentPage === 1 ? 'disabled' : ''}>
-                    <a onClick={() => this.setPage(pager.currentPage - 1)}>&lt;</a>
-                </li>
-                <li className={pager.currentPage === pager.totalPages ? 'disabled' : ''}>
-                    <a onClick={() => this.setPage(pager.currentPage + 1)}>&gt;</a>
-                </li>
-            </ul>
-        );
-    }
+    return (
+      <ul className="pagination">
+        <li className={pager.currentPage === 1 ? 'disabled' : ''}>
+          <a onClick={() => this.setPage(pager.currentPage - 1)}>&lt;</a>
+        </li>
+        <li className={pager.currentPage === pager.totalPages ? 'disabled' : ''}>
+          <a onClick={() => this.setPage(pager.currentPage + 1)}>&gt;</a>
+        </li>
+        <li>
+          <form onSubmit={this.handleSubmit}>
+					<input type="text" value={this.state.value} onChange={this.handleChange} />
+						<input type="submit" value="Submit" />
+					</form>
+        </li>
+      </ul>
+    );
+  }
 }
 
 Pagination.propTypes = propTypes;
